aa6d10b2dae26543c46492c17ef279877c12f057
a-latypov <eziklexa@mail.ru>
2016-09-18 15:15:43 +0500

    барбершоп
    разметка сайта

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-hello world
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+      <title>Барбершоп "Бородинский"</title>
+  </head>
+  <body>
+    <header class="main-header">
+      <div class="container">
+<!--  nav это основгая навигация по сайту-->
+        <nav class="main-navigation">
+        
+        </nav>
+<!--        кнопка входа-->
+        <div class="user-block">
+        
+        </div>
+       
+      </div>  
+    </header>
+  
+  </body>
+</html>
